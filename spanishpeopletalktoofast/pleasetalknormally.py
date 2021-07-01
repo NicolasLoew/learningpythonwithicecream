70e5dd7fa4757af46ae099a2c19322130c309440
Nicolas Loew <ympker@web.de>
2021-07-01 15:08:15 +0200

    Update pleasetalknormally.py
    Updated pseudocode

--- spanishpeopletalktoofast/pleasetalknormally.py
+++ spanishpeopletalktoofast/pleasetalknormally.py
@@ -3,3 +3,10 @@
 
 #While Spanish People talk, make sure they don't talk too fast. If they talk too fast (and they always do), slow down.
 #Startcoding please
+
+#Let's first create some variables: "normal_speed" for the Normal Speed and average person should have (maybe set value to integer 1), 
+#create variable "too_fast" for too much speed (integer 5 which stands for 5x speed) and create the variable "spanish_speed" and set spanish speed equal to too_fast.
+
+
+
+#Now, create a function that checks if spanish speed is too fast, and if it is (which it will be, as defined above) change speed to normal speed and print the new value spanish_speed.
